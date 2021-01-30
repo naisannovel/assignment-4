ed176d2952901e7d4762d396b2e8f23ee5150171
naisannovel <naisannovel@gmail.com>
2021-01-30 20:19:42 +0600

    assignment update

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-document.getElementById('f-class-plus-btn').addEventListener('click',function(){
-    ticketQuantity('f-class-inp',true,'f-class-inp','economy-input');
+document.getElementById('f-class-plus-btn').addEventListener('click', function () {
+    ticketQuantity('f-class-inp', true, 'f-class-inp', 'economy-input');
     // 1st two parameters declare for increase and decrease quantity number.
     // 2nd two parameters declare for to get quantity value. so that we can calculate total cost by totalCost function.these 2 parameters have been declared to pass into the totalCost function.
     // the functions are given below.
 });
 
-document.getElementById('f-class-minus-btn').addEventListener('click',function(){
-    ticketQuantity('f-class-inp',false,'f-class-inp','economy-input');
+document.getElementById('f-class-minus-btn').addEventListener('click', function () {
+    ticketQuantity('f-class-inp', false, 'f-class-inp', 'economy-input');
 });
 
-document.getElementById('economy-Plus-Btn').addEventListener('click',function(){
-    ticketQuantity('economy-input',true,'f-class-inp','economy-input');
+document.getElementById('economy-Plus-Btn').addEventListener('click', function () {
+    ticketQuantity('economy-input', true, 'f-class-inp', 'economy-input');
 });
 
-document.getElementById('economy-minus-Btn').addEventListener('click',function(){
-    ticketQuantity('economy-input',false,'f-class-inp','economy-input');
+document.getElementById('economy-minus-Btn').addEventListener('click', function () {
+    ticketQuantity('economy-input', false, 'f-class-inp', 'economy-input');
 });
 
 
@@ -23,18 +23,18 @@ document.getElementById('economy-minus-Btn').addEventListener('click',function()
 
 // item quantity calculation.
 
-function ticketQuantity(quantityInputId, isIncrease, firstTicketQuantityInputId, economyTicketQuantityInputId){
+function ticketQuantity(quantityInputId, isIncrease, firstTicketQuantityInputId, economyTicketQuantityInputId) {
     let quantityInput = document.getElementById(quantityInputId);
     let quantityInputNumber = parseInt(quantityInput.value);
     let quantityInputNumberNewCount = quantityInputNumber;
 
     // quantity plus minus calculation.
 
-    if(isIncrease == true){
+    if (isIncrease == true) {
         quantityInputNumberNewCount = quantityInputNumber + 1;
     }
-    if(isIncrease == false && quantityInputNumber > 0){
-        quantityInputNumberNewCount = quantityInputNumber - 1; 
+    if (isIncrease == false && quantityInputNumber > 0) {
+        quantityInputNumberNewCount = quantityInputNumber - 1;
     }
 
     quantityInput.value = quantityInputNumberNewCount;
@@ -42,13 +42,13 @@ function ticketQuantity(quantityInputId, isIncrease, firstTicketQuantityInputId,
     // call subtotal, vat, total calculation function.
     // last two parameters pass into the totalCost function.
 
-    totalCost(firstTicketQuantityInputId,economyTicketQuantityInputId);
+    totalCost(firstTicketQuantityInputId, economyTicketQuantityInputId);
 };
 
 
 // subtotal, vat, total calculation.
 
-function totalCost(firstTicketQuantityInputId, economyTicketQuantityInputId){
+function totalCost(firstTicketQuantityInputId, economyTicketQuantityInputId) {
 
     // subtotal calculation.
 
@@ -57,13 +57,13 @@ function totalCost(firstTicketQuantityInputId, economyTicketQuantityInputId){
     let subTotal = firstTicketQuantity * 150 + economyTicketQuantity * 100;
     let subTotalValue = document.getElementById('subtotal');
     subTotalValue.innerText = subTotal;
-    
+
     // vat calculation.
 
     let vat = subTotal * 0.1;
     let totalVat = document.getElementById('vat');
     totalVat.innerText = vat;
-    
+
     // total amount.
 
     let totalSum = document.getElementById('total');
@@ -73,49 +73,53 @@ function totalCost(firstTicketQuantityInputId, economyTicketQuantityInputId){
 
 
 
-// extra section
+// bonus section
 
-document.getElementById('book-now-btn').addEventListener('click',function(){
+document.getElementById('book-now-btn').addEventListener('click', function () {
+
     document.getElementById('main-content').style.display = 'none';
-    document.getElementById('extra-section').style.display = 'block';
-    document.getElementById('background').style.backgroundAttachment = 'fixed';
+    document.getElementById('bonus-section').style.display = 'block';
+    document.getElementById('body-background').style.backgroundAttachment = 'fixed';
 
-    let firstClassSeat = document.getElementById('f-class-inp').value;
-    let economyClassSeat = document.getElementById('economy-input').value;
+    let firstClassSeatQuantityValue = document.getElementById('f-class-inp').value;
+    let economyClassSeatQuantityValue = document.getElementById('economy-input').value;
 
-    if(firstClassSeat == 0 && economyClassSeat == 0){
-       let x = document.getElementById('extra-quantity-show');
-       x.innerText = `you did not book any seat`;
-    }
-    else if(firstClassSeat > 0 && economyClassSeat == 0){
-        let x = document.getElementById('extra-quantity-show');
-       x.innerText = `you booked only first class ${firstClassSeat} seat`
-    }
-    else if(firstClassSeat == 0 && economyClassSeat > 0){
-        let x = document.getElementById('extra-quantity-show');
-       x.innerText = `you booked only economy ${economyClassSeat} seat`
-    }
-    else{
-        let x = document.getElementById('extra-quantity-show');
-       x.innerText = `you booked ${firstClassSeat} first class and ${economyClassSeat} economy seat`
-    }
-    if(firstClassSeat > 0 || economyClassSeat > 0){
-        let mno = document.getElementById('success-message');
-        mno.innerText = `success`;
-        mno.style.color ='green';
-    }
-    else{
-        let mno = document.getElementById('success-message');
-        mno.innerText = `sorry!`;
-        mno.style.color ='red';
+    // quantity value display in bonus section.
+
+    if (firstClassSeatQuantityValue == 0 && economyClassSeatQuantityValue == 0) {
+        let confirmMessage = document.getElementById('bonus-quantity-value-display');
+        confirmMessage.innerText = `you did not book any seat.`;
+    } else if (firstClassSeatQuantityValue > 0 && economyClassSeatQuantityValue == 0) {
+        let confirmMessage = document.getElementById('bonus-quantity-value-display');
+        confirmMessage.innerText = `you booked only first class ${firstClassSeatQuantityValue} seat.`
+    } else if (firstClassSeatQuantityValue == 0 && economyClassSeatQuantityValue > 0) {
+        let confirmMessage = document.getElementById('bonus-quantity-value-display');
+        confirmMessage.innerText = `you booked only economy ${economyClassSeatQuantityValue} seat.`
+    } else {
+        let confirmMessage = document.getElementById('bonus-quantity-value-display');
+        confirmMessage.innerText = `you booked ${firstClassSeatQuantityValue} first class and ${economyClassSeatQuantityValue} economy seat.`
     }
 
-    let exsubttl = document.getElementById('subtotal').innerText;
-    document.getElementById('extra-subtotal').innerText = exsubttl;
+    // bonus section confirmation success/sorry message.
 
-    let vat2 = document.getElementById('vat').innerText;
-    document.getElementById('extra-vat').innerText = vat2;
+    if (firstClassSeatQuantityValue > 0 || economyClassSeatQuantityValue > 0) {
+        let successMessage = document.getElementById('bonus-confirmation-text');
+        successMessage.innerText = `success`;
+        successMessage.style.color = 'green';
+    } else {
+        let successMessage = document.getElementById('bonus-confirmation-text');
+        successMessage.innerText = `sorry!`;
+        successMessage.style.color = 'red';
+    };
 
-    let total2 = document.getElementById('total').innerText;
-    document.getElementById('extra-total').innerText = total2;
+    // subTotal, vat, total value passed in bonus section; 
+
+    let subTotalValue = document.getElementById('subtotal').innerText;
+    document.getElementById('bonus-section-subtotal').innerText = subTotalValue;
+
+    let vat = document.getElementById('vat').innerText;
+    document.getElementById('bonus-section-vat').innerText = vat;
+
+    let totalValue = document.getElementById('total').innerText;
+    document.getElementById('bonus-section-total').innerText = totalValue;
 })
